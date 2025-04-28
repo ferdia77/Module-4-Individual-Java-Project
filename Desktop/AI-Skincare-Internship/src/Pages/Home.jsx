85a6302c4f74edf3e93e3cf84b45febc97c40168
David <kingqueensuited22@gmai.com>
2025-04-28 10:57:22 +0200

    a

---
+++ Desktop/AI-Skincare-Internship/src/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <div className="flex">
+        <div>
+            <a className='p-8px'>
+                SKINSTRIC
+            </a>
+            <a className='p-8px'>
+                [INTRO]
+            </a>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home
